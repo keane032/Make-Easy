db56aa3e94f3b4568575319d7a1c0a1cd56ca152
keane032 <keane032@gmail.com>
2019-03-30 15:46:36 -0300

    Criando intent da tela home

--- MKIX/app/src/main/java/br/com/ufc/mkix/Home.java
+++ MKIX/app/src/main/java/br/com/ufc/mkix/Home.java
@@ -1,16 +1,21 @@
 package br.com.ufc.mkix;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity  {
 
     List<String> categorias = new ArrayList<>();
     ListView ViewCategorias;
@@ -32,6 +37,15 @@ public class Home extends AppCompatActivity {
                 this,
                 android.R.layout.simple_list_item_1,
                 categorias);
+
+        ViewCategorias.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int posicao, long id) {
+                String cat = (String) parent.getItemAtPosition(posicao);
+                Toast.makeText(Home.this, cat, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent();
+            }
+        });
 
         ViewCategorias.setAdapter(arrayAdapter);
 
---
+++ MKIX/app/src/main/java/br/com/ufc/mkix/Lista_Trabalhadores.java
@@ -0,0 +1,7 @@
+package br.com.ufc.mkix;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class Lista_Trabalhadores extends AppCompatActivity {
+
+}
