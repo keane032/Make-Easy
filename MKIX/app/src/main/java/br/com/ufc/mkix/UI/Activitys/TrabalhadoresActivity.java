610cefa616b439cfc9a56c54c56c7c506adb42fc
Max Wendel <maxwendelffaraujo@alu.ufc.br>
2019-05-02 01:42:14 -0300

    Um monte de coisa aí

---
+++ MKIX/app/src/main/java/br/com/ufc/mkix/UI/Activitys/TrabalhadoresActivity.java
@@ -0,0 +1,15 @@
+package br.com.ufc.mkix.UI.Activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.ufc.mkix.R;
+
+public class TrabalhadoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trabalhadores);
+    }
+}
