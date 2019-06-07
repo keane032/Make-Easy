5bf6d8f15579d9568e8d7b86144d9b230c584287
Max Wendel <maxwendelffaraujo@alu.ufc.br>
2019-06-07 15:59:05 -0300

    Otimizando o carregamento dos dados

--- MKIX/app/src/main/java/br/com/ufc/mkix/UI/activities/Home.java
+++ MKIX/app/src/main/java/br/com/ufc/mkix/UI/activities/Home.java
@@ -14,7 +14,6 @@ import android.widget.Toast;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -22,6 +21,7 @@ import java.util.List;
 
 import br.com.ufc.mkix.R;
 import br.com.ufc.mkix.model.enums.Categoria;
+import br.com.ufc.mkix.persistence.PersistenceUnit;
 
 public class Home extends AppCompatActivity  {
 
@@ -30,66 +30,36 @@ public class Home extends AppCompatActivity  {
     AutoCompleteTextView autoCompleteTextView;
 
     DatabaseReference mydatabase;
+    PersistenceUnit persistenceUnit = PersistenceUnit.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
-        mydatabase = FirebaseDatabase.getInstance().getReference("categorias");
+        initFirebase();
+        syncronizeDatabase();
+    }
 
-//        mydatabase.child("0").setValue(Categoria.EMPREGADA);
-//        mydatabase.child("1").setValue(Categoria.MORDOMO);
-//        mydatabase.child("2").setValue(Categoria.PEDREIRO);
-//        mydatabase.child("3").setValue(Categoria.PISCINEIRO);
-//        mydatabase.child("4").setValue(Categoria.ENCANADOR);
-//        mydatabase.child("5").setValue(Categoria.DIARISTA);
-//        mydatabase.child("6").setValue(Categoria.COZINHA);
-//        mydatabase.child("7").setValue(Categoria.CHURRASQUEIRO);
-//        mydatabase.child("8").setValue(Categoria.ELETRICISTA);
-//        mydatabase.child("9").setValue(Categoria.CARPINTEIRO);
-//        mydatabase.child("10").setValue(Categoria.BABA);
+    private void initFirebase(){
+        if (mydatabase == null){
+            mydatabase = persistenceUnit.getReference();
+        }
+    }
 
-        mydatabase.addValueEventListener(new ValueEventListener() {
+    private void syncronizeDatabase() {
+        mydatabase.child("categorias").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                categorias.clear();
 
-                //iterating through all the nodes
                 for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
-                    //getting artist
                     Categoria categoria = postSnapshot.getValue(Categoria.class);
-                    //adding artist to the list
                     categorias.add(categoria.toString());
                 }
 
-                autoCompleteTextView = findViewById(R.id.CampoDeBusca);
-
-                ViewCategorias = (ListView) findViewById(R.id.Categorias);
-
-                autoCompleteTextView.setThreshold(1); //will start working from first character
-
-                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                        getBaseContext(),
-                        android.R.layout.simple_list_item_1,
-                        categorias);
-
-                ArrayAdapter<String> AutoarrayAdapter = new ArrayAdapter<String>(
-                        getBaseContext(),
-                        android.R.layout.simple_list_item_1,
-                        categorias);
-
-                autoCompleteTextView.setAdapter(AutoarrayAdapter);
-
-                ViewCategorias.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int posicao, long id) {
-                        String cat = (String) parent.getItemAtPosition(posicao);
-                        listarTrabalhadoresPorCategoria(cat);
-
-                    }
-                });
-
-                ViewCategorias.setAdapter(arrayAdapter);
+                initViewCategorias();
+                initAutoCompleteAdapter();
             }
 
             @Override
@@ -97,10 +67,43 @@ public class Home extends AppCompatActivity  {
                 Toast.makeText(getBaseContext(),"errro",Toast.LENGTH_SHORT).show();
             }
         });
-
-
-
     }
+
+    private void initAutoCompleteAdapter() {
+        ArrayAdapter<String> AutoarrayAdapter =
+                new ArrayAdapter<String>(
+                    getBaseContext(),
+                    android.R.layout.simple_list_item_1,
+                    categorias
+        );
+
+        autoCompleteTextView = findViewById(R.id.CampoDeBusca);
+        autoCompleteTextView.setThreshold(1); //will start working from first character
+        autoCompleteTextView.setAdapter(AutoarrayAdapter);
+    }
+
+    private void initViewCategorias(){
+        ViewCategorias = findViewById(R.id.Categorias);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                getBaseContext(),
+                android.R.layout.simple_list_item_1,
+                categorias);
+
+        ViewCategorias.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int posicao, long id) {
+                String cat = (String) parent.getItemAtPosition(posicao);
+                listarTrabalhadoresPorCategoria(cat);
+
+            }
+
+        });
+
+        ViewCategorias.setAdapter(arrayAdapter);
+    }
+
 
 
     public void buscarPorNome(View view){
