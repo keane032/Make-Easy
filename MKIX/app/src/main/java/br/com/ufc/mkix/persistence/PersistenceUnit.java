50e15001f12d4641b0e66b907bbab3f9a5155d4b
Max Wendel <maxwendelffaraujo@alu.ufc.br>
2019-06-07 15:58:40 -0300

    Criando uma classe Singleton para uma instância única da conexão e persistencia de dados com o FireBase.

---
+++ MKIX/app/src/main/java/br/com/ufc/mkix/persistence/PersistenceUnit.java
@@ -0,0 +1,39 @@
+package br.com.ufc.mkix.persistence;
+
+import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/***
+ * Essa classe implementa o padrão de projeto Singleton
+ * para possuir somente uma instância de conexão com o fb
+ */
+public class PersistenceUnit extends Application {
+
+    private static PersistenceUnit persistenceUnit;
+
+    private final FirebaseDatabase firebaseDatabase;
+
+    private PersistenceUnit(){
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseDatabase.setPersistenceEnabled(true);
+    }
+
+    public static synchronized PersistenceUnit getInstance(){
+        if (persistenceUnit == null){
+            persistenceUnit = new PersistenceUnit();
+        }
+
+        return persistenceUnit;
+    }
+
+    public DatabaseReference getReference(){
+        return firebaseDatabase.getReference();
+    }
+
+
+
+}
