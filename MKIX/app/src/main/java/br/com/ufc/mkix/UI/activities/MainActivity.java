18217b5fa54b0064b708bddc76097c1652c478fa
Max Wendel <maxwendelffaraujo@alu.ufc.br>
2019-06-21 10:29:31 -0300

    Realizando o cadastro com o Google Sign In

--- MKIX/app/src/main/java/br/com/ufc/mkix/UI/activities/MainActivity.java
+++ MKIX/app/src/main/java/br/com/ufc/mkix/UI/activities/MainActivity.java
@@ -1,21 +1,46 @@
 package br.com.ufc.mkix.UI.activities;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import br.com.ufc.mkix.R;
 import br.com.ufc.mkix.UI.adapters.Sobre;
 
 public class MainActivity extends AppCompatActivity {
 
+    int RC_SIGN_IN = 0;
+    SignInButton signInButton;
+    GoogleSignInClient mGoogleSignInClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton = findViewById(R.id.sign_in_button);
+
+        signInButton.setOnClickListener((view) -> { signIn();});
     }
 
 
@@ -24,9 +49,47 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private void signIn(){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent,RC_SIGN_IN);
+    }
+
     public void cadastrar(View view){
         Intent intent = new Intent(this, ClienteActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completeTask) {
+        try{
+            GoogleSignInAccount account = completeTask.getResult(ApiException.class);
+            startActivity(new Intent(this,Home.class));
+        }catch (ApiException e){
+            Log.w("Google Sign In Error","signInResult: failed code ="+e.getStatusCode());
+            Toast.makeText(this,"Não foi possivel realizar o cadastro",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if (account != null){
+            startActivity(new Intent(MainActivity.this,Home.class));
+        }
+
+        super.onStart();
     }
 
     @Override
@@ -38,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.navigation_about){
-            Intent intent = new Intent(this, Sobre.class);
+            Intent intent = new Intent(MainActivity.this, Sobre.class);
             startActivity(intent);
         }
 
