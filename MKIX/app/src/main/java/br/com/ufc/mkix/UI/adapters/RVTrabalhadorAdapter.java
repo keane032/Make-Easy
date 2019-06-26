baa5d7bd5cb1e41f81fc5ded689c4dc6944af0b0
keane032 <keane032@gmail.com>
2019-06-26 00:00:45 -0300

    bug do card resolvido

--- MKIX/app/src/main/java/br/com/ufc/mkix/UI/adapters/RVTrabalhadorAdapter.java
+++ MKIX/app/src/main/java/br/com/ufc/mkix/UI/adapters/RVTrabalhadorAdapter.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,6 +33,7 @@ public class RVTrabalhadorAdapter extends RecyclerView.Adapter<RVTrabalhadorAdap
     @NonNull
     @Override
     public TrabalhadorViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        Log.d("ids",""+ i);
         View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.worker_card, viewGroup, false);
         TrabalhadorViewHolder tvh = new TrabalhadorViewHolder(v,this.trabalhadores);
         return tvh;
@@ -39,12 +41,43 @@ public class RVTrabalhadorAdapter extends RecyclerView.Adapter<RVTrabalhadorAdap
 
     @Override
     public void onBindViewHolder(@NonNull TrabalhadorViewHolder trabalhadorViewHolder, int i) {
+        Log.d("wqqwqe",""+ i);
         trabalhadorViewHolder.tvNome.setText(trabalhadores.get(i).getNome());
         trabalhadorViewHolder.tvDescricao.setText(trabalhadores.get(i).getDescricao());
         Picasso.get()
                 .load(trabalhadores.get(i).getPhotoId())
                 .error(R.drawable.ic_launcher_background)
                 .into(trabalhadorViewHolder.ivPhoto);
+        trabalhadorViewHolder.cv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Log.d("wqqwqe","");
+                Intent intent = new Intent(context, TrabalhadorInfoActivitiy.class);
+
+                Toast toast = Toast.makeText(context, "",Toast.LENGTH_SHORT);
+                toast.show();
+
+                StringBuilder skills = new StringBuilder();
+
+                intent.putExtra("nome",trabalhadores.get(i).getNome());
+                intent.putExtra("email",trabalhadores.get(i).getEmail());
+                intent.putExtra("contato",trabalhadores.get(i).getContatos().get(i).getNumero());
+                intent.putExtra("photoId",trabalhadores.get(i).getPhotoId());
+                intent.putExtra("position",trabalhadores.get(i).getPosition().toString());
+
+
+                for (Categoria skill: trabalhadores.get(i).getSkills()) {
+                    skills.append(skill.name());
+                    skills.append(",");
+                }
+
+                intent.putExtra("skills",skills.toString());
+
+                context.startActivity(intent);
+            }
+        });
+
     }
 
     @Override
@@ -72,7 +105,7 @@ public class RVTrabalhadorAdapter extends RecyclerView.Adapter<RVTrabalhadorAdap
                 @Override
                 public void onClick(View v) {
                     Context context = v.getContext();
-
+                    Log.d("wqqwqe","");
                     Intent intent = new Intent(context, TrabalhadorInfoActivitiy.class);
 
                     Toast toast = Toast.makeText(context, "",Toast.LENGTH_SHORT);
